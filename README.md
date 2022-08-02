4cf4105d8d255a7d8c0413123c7619ef8e0c7576
S3mlin <lsemic97@gmail.com>
2022-08-02 14:54:33 +0200

    clean

--- README.md
+++
@@ -1,3 +0,0 @@
-# To do list!
-
-A simple web app on which you can add, delete and edit tasks on your to do list!
--- app/forms.py
+++
@@ -1,8 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
-
-
-class TodoForm(FlaskForm):
-    task = StringField('Task', validators=[DataRequired()])
-    submit = SubmitField('Add task into to do list!')
